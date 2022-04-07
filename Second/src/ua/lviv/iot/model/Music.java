8bb67c965b2aab150a09bc1c203ccd6549b86b44
Hydra <little.hydron@gmail.com>
2022-04-07 13:35:08 +0300

    Added code for second lab

---
+++ Second/src/ua/lviv/iot/model/Music.java
@@ -0,0 +1,37 @@
+package ua.lviv.iot.model;
+
+public class Music {
+	private String genre, name, author;
+	private double durationInMinutes, sizeInMb;
+
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getDurationInMinutes() {
+		return durationInMinutes;
+	}
+	public void setDurationInMinutes(double durationInMinutes) {
+		this.durationInMinutes = durationInMinutes;
+	}
+	public double getSizeInMb() {
+		return sizeInMb;
+	}
+	public void setSizeInMb(double sizeInMb) {
+		this.sizeInMb = sizeInMb;
+	}
+}
